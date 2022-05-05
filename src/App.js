65a6baff82cdfca1b439732838796024fc473927
Charlie Mac <coding.0awnx@aleeas.com>
2022-05-05 21:35:47 +1000

    add shop page, collection preview component and collection item component, add styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { Routes, Route } from "react-router-dom"
+
 import Homepage from "./pages/homepage/homepage.component"
+import ShopPage from "./pages/shop/shop.component"
 
 import "./App.css"
 
@@ -6,7 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <Homepage />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/shop" element={<ShopPage />} />
+        </Routes>
       </div>
     </>
   )
